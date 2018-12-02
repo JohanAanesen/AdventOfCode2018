bc0ceb300418ad76526cd9df18cedf777b34f858
Johan Aanesen <johanaan@stud.ntnu.no>
2018-12-02 12:00:28 +0100

    day 2 done

--- Day 2/main.go
+++ Day 2/main.go
@@ -20,9 +20,12 @@ func main() {
 	twos := 0
 	threes := 0
 	var letters []string
+	var list []string
 
 	for r.Scan() {
 		path := r.Text()
+
+		list = append(list, path)
 
 		letters = strings.Split(path, "")
 
@@ -39,9 +42,37 @@ func main() {
 				break
 			}
 		}
-
-
 	}
 
 	fmt.Printf("Part 1: %v\n", twos*threes)
+
+	for i, line := range list{
+		for j, line2 := range list{
+			if i != j{
+				checkWord(line, line2)
+			}
+		}
+	}
+}
+
+func checkWord(word1 string, word2 string){
+	w1 := strings.Split(word1, "")
+	w2 := strings.Split(word2, "")
+
+	diff := 0
+
+	for i, letter := range w2{
+		if w1[i] != letter{
+			diff++
+			w1[i] = ""
+			w2[i] = ""
+			if diff > 1{
+				return
+			}
+		}
+	}
+
+	fmt.Printf("Part 2: %v\n", strings.Join(w1, ""))
+
+	os.Exit(0) //quit
 }
