0fe6a1a98454f8195c1256107b691e14dcd37e3c
Johan Aanesen <johanaan@stud.ntnu.no>
2018-12-05 10:01:19 +0100

    day 5 done, slow af tho

--- Day 5/main.go
+++ Day 5/main.go
@@ -18,31 +18,82 @@ func main() {
 	r := bufio.NewScanner(b)
 
 	var input []string
+	var input2 []string
+	var inputString string
 
 	for r.Scan() {
 		line := r.Text()
 
 		input = strings.Split(line, "")
+		input2 = strings.Split(line, "")
+		inputString = line
 	}
 
-	for len(input) > 10{
-		for i:=0; i < len(input)-1; i++{
-			if input[i] == strings.ToLower(input[i]){
-				if input[i+1] == strings.ToUpper(input[i+1]) && strings.ToUpper(input[i]) ==  strings.ToUpper(input[i+1]){
-					input = append(input[:i], input[i+2:]...)
+	part1 := retract(input)
+	fmt.Printf("Part 1: %v\n", part1)
 
+	var types = make(map[string]bool)
+	var results = make(map[string]int)
+
+	for _, val := range input2{
+		types[strings.ToLower(val)] = true
+	}
+
+	for val, _ := range types{
+		tempString := strings.Replace(inputString, val, "", -1)
+		tempString = strings.Replace(tempString, strings.ToUpper(val), "", -1)
+		var tempArr []string
+		tempArr = strings.Split(tempString, "")
+
+		/*
+		for i:=0; i < len(tempArr); i++{
+			if strings.ToLower(tempArr[i]) == val{
+				tempArr = append(tempArr[:i], tempArr[i+1:]...) //delete all instances of the type
+			}
+		}*/
+
+		result := retract(tempArr)
+		//fmt.Printf("Val: %s, AND STRING: %s\n", val, tempString)
+		results[val] = result
+	}
+
+	smallest := 999999
+	removedType := ""
+	for i, val := range results{
+		if val < smallest{
+			smallest = val
+			removedType = i
+		}
+	}
+
+	fmt.Printf("Part 2: Removed %s, retracted length now %v\n", removedType, smallest)
+
+}
+
+func retract(slice []string)int{
+	retracted := true
+
+	for retracted{
+		finished := true
+		for i:=0; i < len(slice)-1; i++{
+			if slice[i] == strings.ToLower(slice[i]){
+				if slice[i+1] == strings.ToUpper(slice[i+1]) && strings.ToUpper(slice[i]) ==  strings.ToUpper(slice[i+1]){
+					slice = append(slice[:i], slice[i+2:]...)
+					finished = false
 				}
-			}else if input[i] == strings.ToUpper(input[i]){
-				if input[i+1] == strings.ToLower(input[i+1]) && strings.ToUpper(input[i]) ==  strings.ToUpper(input[i+1]){
-					input = append(input[:i], input[i+2:]...)
-
+			}else if slice[i] == strings.ToUpper(slice[i]){
+				if slice[i+1] == strings.ToLower(slice[i+1]) && strings.ToUpper(slice[i]) ==  strings.ToUpper(slice[i+1]){
+					slice = append(slice[:i], slice[i+2:]...)
+					finished = false
 				}
 			}
 		}
-		fmt.Println(len(input))
+		if finished{
+			return len(slice)
+			retracted = false
+		}
+
 	}
 
-
-
-	fmt.Println(input)
+	return 0
 }
